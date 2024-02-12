ee12478f38886891ade2fb8a9af5aa75e68c0730
Kirill Shishov <skireal@mail.ru>
2024-02-12 22:15:50 +0300

    fix: incorrect resource string

--- src/utils/httpServer.ts
+++ src/utils/httpServer.ts
@@ -5,21 +5,21 @@ import {
   createUser,
   updateUser,
   deleteUser,
-} from '../controllers/usersController.js';
+} from '../controllers/usersController';
 
 export const handleRequest = (req: IncomingMessage, res: ServerResponse) => {
   const { method, url } = req;
   const [, , resource, id] = url?.split('/') || [];
 
-  if (method === 'GET' && resource === 'api/users' && id) {
+  if (method === 'GET' && resource === 'users' && id) {
     return getUserById(req, res, id);
-  } else if (method === 'GET' && resource === 'api/users') {
+  } else if (method === 'GET' && resource === 'users') {
     return getUsers(req, res);
-  } else if (method === 'POST' && resource === 'api/users') {
+  } else if (method === 'POST' && resource === 'users') {
     return createUser(req, res);
-  } else if (method === 'PUT' && resource === 'api/users' && id) {
+  } else if (method === 'PUT' && resource === 'users' && id) {
     return updateUser(req, res, id);
-  } else if (method === 'DELETE' && resource === 'api/users' && id) {
+  } else if (method === 'DELETE' && resource === 'users' && id) {
     return deleteUser(req, res, id);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
